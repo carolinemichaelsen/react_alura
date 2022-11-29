81a7f2d8cc3d83b0ceff49b1dcaf81e3c9b91cb9
Caroline Michaelsen <117784264+carolinemichaelsen@users.noreply.github.com>
2022-11-29 14:56:25 -0300

    aula 4 - montando os times

--- organograma/src/componentes/Formulario/index.js
+++ organograma/src/componentes/Formulario/index.js
@@ -6,20 +6,10 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'Mobile',
-        'Inovação e Gestão',
-    ]
-
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState('')
-
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
@@ -29,42 +19,46 @@ const Formulario = (props) => {
             imagem,
             time
         })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto 
-                obrigatorio={true} 
-                label="Nome" 
-                placeholder="Digite seu nome"
-                valor={nome}
-                aoAlterado={valor => setNome(valor)}
-            />
-            <CampoTexto 
-                obrigatorio={true}
-                label="Cargo"
-                placeholder="Digite seu cargo"
-                valor={cargo}
-                aoAlterado={valor => setCargo(valor)}
-            />
-            <CampoTexto 
-                label="Imagem" 
-                placeholder="Digite o endereço da imagem"
-                valor={imagem}
-                aoAlterado={valor => setImagem(valor)}
-            />
-            <ListaSuspensa 
-                obrigatorio={true}
-                label="Time"
-                itens={times}
-                valor={time}
-                aoAlterado={valor => setTime}
-            />
-            <Botao>
-                Criar Card
-            </ Botao>
+                <CampoTexto 
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo" 
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem" 
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
